222fc97df8ef3b4b5a7c9bde39c24e3ac7b8c54b
Fabio Viola <desmovalvo@gmail.com>
2018-05-22 17:52:51 +0100

    Added Europeana WT

---
+++ europeanaWT/europeanaWT-NG.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python3
+
+# reqs
+from sepy.SEPAClient import *
+from sepy.YSAPObject import *
+from lib.EuropeHandler import *
+import configparser
+import logging
+            
+# main
+if __name__ == "__main__":
+
+    ##############################################################
+    #
+    # Initialization
+    #
+    ##############################################################
+
+    # initialize the logging system
+    logger = logging.getLogger('europeanaWT')
+    logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
+    logging.debug("Logging subsystem initialized")
+    
+    # read europeana key
+    config = configparser.ConfigParser()
+    config.read("europeanaWT.conf")
+    clientID = config["Europeana"]["clientId"]
+    searchLimit = config["Europeana"]["limit"]
+
+    # create a new KP
+    kp = SEPAClient(None, 40)
+
+    # create an YSAPObject
+    ysap = YSAPObject("europeanaTD.yaml", 40)
+    
+    # create URIs for
+    # - thing
+    # - thingDescription
+    # - search action
+    # - search action input and output dataschema
+    thingURI = ysap.getNamespace("qmul") + "EuropeanaWT"
+    thingDescURI = ysap.getNamespace("qmul") + "EuropeanaWT_TD"
+    actionURI = ysap.getNamespace("qmul") + "searchAction"
+    indataSchemaURI = ysap.getNamespace("qmul") + "searchAction_IDS"
+    outdataSchemaURI = ysap.getNamespace("qmul") + "searchAction_ODS"
+
+    ##############################################################
+    #
+    # Put the Thing Description into SEPA
+    #
+    ##############################################################
+    
+    # get the first update (TD_INIT)
+    updText = ysap.getUpdate("TD_INIT",
+                             {"thingURI": " <%s> " % thingURI,
+                              "thingDescURI": " <%s> " % thingDescURI,
+                              "thingName":" 'EuropeanaWT' "})
+    kp.update(ysap.updateURI, updText)
+    
+    # get the second update (TD_ADD_ACTION_STRING_INPUT_GRAPH_OUTPUT)
+    updText = ysap.getUpdate("TD_ADD_ACTION_STRING_INPUT_GRAPH_OUTPUT",
+                             {"thingDescURI": " <%s> " % thingDescURI,
+                              "actionURI": " <%s> " % actionURI,
+                              "actionName": " 'searchByTags' ",                              
+                              "inDataSchema": " <%s> " % indataSchemaURI,
+                              "outDataSchema": " <%s> " % outdataSchemaURI,
+                              "actionComment": " 'Search song by tags' " })
+    kp.update(ysap.updateURI, updText)
+        
+    # # remove existing action instances
+    # kp.update(updateURI, remove_actions.format(
+    #     actionURI = actionURI
+    # ))
+
+    ##############################################################
+    #
+    # Subscribe to actions
+    #
+    ##############################################################
+
+    # subscribe
+    subText = ysap.getQuery("ACTION_REQUESTS",
+                            {"thingURI": " <%s> " % thingURI,
+                             "thingDescURI": " <%s> " % thingDescURI,
+                             "actionURI": " <%s> " % actionURI })
+    kp.subscribe(ysap.subscribeURI, subText, "actions", EuropeHandler(kp, ysap, clientID, searchLimit))
+
+    ##############################################################
+    #
+    # Wait for the end...
+    #
+    ##############################################################    
+
+    logging.info("WebThing ready! Waiting for actions!")
+    try:
+       input("Press <ENTER> to close the WebThing")
+       logging.debug("Closing WebThing")
+
+       # delete action and instances
+       updText = ysap.getUpdate("TD_DELETE_ACTION_STRING_INPUT_GRAPH_OUTPUT",
+                                {"thingDescURI": " <%s> " % thingDescURI,
+                                 "actionURI": " <%s> " % actionURI,
+                                })
+       kp.update(ysap.updateURI, updText)
+       
+       # delete thing description
+       updText = ysap.getUpdate("TD_DELETE",
+                                {"thingURI": " <%s> " % thingURI})
+       kp.update(ysap.updateURI, updText)
+       
+    except KeyboardInterrupt:
+       logging.debug("Closing WebThing")
---
+++ europeanaWT/lib/EuropeHandler.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python3
+
+# reqs
+import json
+import rdflib
+import logging
+import requests
+from termcolor import colored
+
+# constants
+NAMESEARCH_URL = "https://www.europeana.eu/api/v2/search.json?wskey=%s&query=%s&profile=rich"
+
+class EuropeHandler:
+
+    def __init__(self, kp, ysap, clientID, limit):
+
+        # initialize the logging system
+        logger = logging.getLogger('freesoundWT')
+        logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.DEBUG)
+        logging.debug("Logging subsystem initialized for JamHandler")
+
+        # initialize attributes
+        self.clientID = clientID
+        self.limit = limit
+        self.counter = 0
+        self.ysap = ysap
+        self.kp = kp
+
+
+    def handle(self, added, removed):
+
+        ##############################################################
+        #
+        # check if it is the confirm message
+        #
+        ##############################################################    
+        
+        if (self.counter == 0):
+
+            # debug message
+            logging.info("Subscription to actions correctly initialized")
+
+        ##############################################################
+        #
+        # ...or a notification
+        #
+        ##############################################################    
+            
+        else:
+            
+            # debug message
+            logging.info("Search request #%s" % self.counter)
+            
+            # cycle over added bindings
+            triples = []
+            instanceURI = None
+            outputGraph = None
+            for a in added:
+
+                ##############################################################
+                #
+                # read the input
+                #
+                ##############################################################    
+                
+                # read the action configuration
+                # inValue is a string containing tags
+                # outValue is the URI for a graph
+                instanceURI = a["actionInstance"]["value"]
+                inputData = json.loads(a["inValue"]["value"])
+                outputGraph = a["outValue"]["value"]
+                logging.info("Action output will be in %s" % outputGraph)
+
+                ##############################################################
+                #
+                # search on freesound
+                #
+                ##############################################################    
+            
+                searchPattern = "+OR+".join(inputData["tags"])
+                r = requests.get(NAMESEARCH_URL % (self.clientID, searchPattern))
+                print(colored(searchPattern, "blue", attrs=[]))
+                logging.info("Asking Europeana for songs matching %s" % searchPattern)
+                res = json.loads(r.text)
+                for r in res["items"]:
+                    logging.info(r["title"])
+
+                # experimental part -- contacting the local sparql generate server
+                searchuri = NAMESEARCH_URL % (self.clientID, searchPattern)   
+                query = """PREFIX ac: <http://audiocommons.org/ns/audiocommons#>
+                PREFIX dc: <http://purl.org/dc/elements/1.1/>
+                PREFIX iter: <http://w3id.org/sparql-generate/iter/>
+                PREFIX fn: <http://w3id.org/sparql-generate/fn/>
+                PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>                
+                GENERATE { 
+                  ?audioClip rdf:type ac:AudioClip .
+                  ?audioClip ac:available_as ?audioFile .
+                  ?audioClip dc:title ?title .
+                  ?audioFile rdf:type ac:AudioFile
+                }
+                SOURCE <%s> AS ?source
+                ITERATOR iter:JSONPath(?source,"$..items[*]") AS ?res
+                WHERE {
+                BIND(IRI(fn:JSONPath(?res, "link")) AS ?audioClip)
+                BIND(fn:JSONPath(?res, "type") AS ?type)
+                BIND(fn:JSONPath(?res, "title[0]") AS ?title)  
+                BIND(IRI(fn:JSONPath(?res, "edmIsShownBy[0]")) AS ?audioFile)
+  	        FILTER(?type = "SOUND")
+                } LIMIT %s""" % (searchuri, self.limit)
+                print(searchuri)
+                
+                response = requests.post('http://localhost:5000/sparqlgen', data={"query":query})
+                sg_res = json.loads(response.text)
+                print(sg_res["result"])
+
+                g = rdflib.Graph()
+                g.parse(data=sg_res["result"], format="n3")
+                for triple in g:
+                    triple_string = " "
+                    for field in triple:
+                        if isinstance(field, rdflib.term.URIRef):
+                            triple_string += " <%s> " % field
+                        elif isinstance(field, rdflib.term.BNode):
+                            triple_string += " _:%s " % field
+                        else:
+                            triple_string += " '%s' " % field.replace("'", "\\'")
+                    triples.append(triple_string)
+                
+            # put results into SEPA
+            if len(removed) == 0:
+                tl = ".".join(triples)
+                updText = self.ysap.getUpdate("INSERT_SEARCH_RESPONSE",
+                                              { "graphURI": " <%s> " % outputGraph,
+                                                "instanceURI": " <%s> " % instanceURI,
+                                                "tripleList": tl })
+                print(updText)
+                self.kp.update(self.ysap.updateURI, updText)                                               
+                logging.info("Task completed!")
+                
+                
+        # increment counter
+        self.counter += 1
